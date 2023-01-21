44bff2076461ec97bc69cd5955cd285aa0ff0c9e
Ryan Dong <ryandong@Ryans-MacBook-Pro-2.local>
2023-01-21 01:37:12 -0800

    added connection

--- db/connection.js
+++ db/connection.js
@@ -1,27 +1,46 @@
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const db = mysql.createConnection(
+//     {
+//         host: 'localhost',
+//         user: 'root',
+//         password: process.env.DB_PASSWORD,
+//         database: 'employee_tracker_db'
+//     },
+//     console.log(`Connected to the employee_tracker database.`)
+// );
+
+// const pool = mysql.createPool(
+//     {
+//     host: 'localhost'
+//     user: ' root',
+//     password: process.env.DB_PASSWORD,
+//     database: 'employee_tracker_db'
+//     }
+// );
+
+// const promisePool = pool.promise();
+
+// db.connect();
+
+// module.exports = { db: db, pool: pool, promisePool: promisePool };
+
+
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: process.env.DB_PASS,
-        database: 'employee_tracker'
-    },
-    console.log(`Connected to the employee_tracker database.`)
-);
-
-const pool = mysql.createPool(
-    {
-    host: 'localhost'
-    user: ' root',
-    password: process.env.DB_PASS,
-    database: 'employee_tracker'
-    }
-);
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employee_tracker_db'
+});
 
 const promisePool = pool.promise();
 
-db.connect();
+pool.on('connection', (connection) => {
+    console.log(`Connected to the employee_tracker database.`);
+});
 
-module.exports = { db: db, pool: pool, promisePool: promisePool };
+module.exports = { pool: pool, promisePool: promisePool };
